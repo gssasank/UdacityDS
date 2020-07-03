2bce12894a105df3c85093ae04ea15519911a909
agustuz <gssasank@gmail.com>
2020-07-03 18:32:06 +0530

    Enter

--- DataStructures/LinkedLists/ReverseLL.py
+++ DataStructures/LinkedLists/ReverseLL.py
@@ -22,10 +22,56 @@ class LinkedList:
         node = self.head
         while node:
             yield node.value
-            node = node.next
+            node = node.next # THIS IS AN AMAZING LINE OF CODE!!!! IT USES THE CONCEPT OF GENERATORS
             
     def __repr__(self):
         return str([v for v in self])
     
     
     
+# Solution
+
+# Time complexity O(N)
+def reverse(linked_list):
+    """
+    Reverse the inputted linked list
+
+    Args:
+       linked_list(obj): Linked List to be reversed
+    Returns:
+       obj: Reveresed Linked List
+    """
+    new_list = LinkedList()
+    
+    prev_node = None
+
+    """
+    A simple idea - Pick a node from the original linked list traversing form the beginning, and 
+    prepend it to the new linked list. 
+    We have to use a loop to iterate over the nodes of original linked list
+    """
+    # In this "for" loop, the "value" is just a variable whose value will be updated in each iteration
+    for value in linked_list:
+        # create a new node
+        new_node = Node(value)
+        
+        # Make the new_node.next point to the 
+        # node created in previous iteration
+        new_node.next = prev_node 
+        
+        # This is the last statement of the loop
+        # Mark the current new node as the "prev_node" for next iteration
+        prev_node = new_node
+    
+    # Update the new_list.head to point to the final node that came out of the loop
+    new_list.head = prev_node
+    
+    return new_list
+
+llist = LinkedList()
+for value in [4,2,5,1,-3,0]:
+    llist.append(value)
+
+flipped = reverse(llist)
+is_correct = list(flipped) == list([0,-3,1,5,2,4]) and list(llist) == list(reverse(flipped))
+print("Pass" if is_correct else "Fail")
