a1cec1709ef146d1500f1a0e4e5e6ad24e75748b
agustuz <gssasank@gmail.com>
2020-07-06 17:00:36 +0530

    Enter

---
+++ DataStructures/Trees/CreatingBinaryTree.py
@@ -0,0 +1,74 @@
+class Node:
+    def __init__(self, value = None):
+        self.value = value
+        self.left = None
+        self.right = None
+    
+    def get_value(self):
+        return self.value
+    
+    def set_value(self, value):
+        self.value = value
+    
+    def set_left_child(self, node):
+        self.left = node
+    
+    def set_right_child(self, node):
+        self.right = node
+        
+    def get_left_child(self):
+        return self.left
+
+    def get_right_child(self):
+        return self.right
+        
+    def has_left_child(self):
+        if self.left == None:
+            return False
+        else:
+            return True
+    
+    def has_right_child(self):
+        if self.right == None:
+            return False
+        else:
+            return True
+        
+class Tree:
+    def __init__(self, value = None):
+        self.root = Node(value)
+    
+    def get_root(self):
+        return self.root
+    
+
+## check
+
+node0 = Node("apple")
+node1 = Node("banana")
+node2 = Node("orange")
+
+print(f"has left child? {node0.has_left_child()}")
+print(f"has right child? {node0.has_right_child()}")
+
+print("adding left and right children")
+node0.set_left_child(node1)
+node0.set_right_child(node2)
+
+print(f"has left child? {node0.has_left_child()}")
+print(f"has right child? {node0.has_right_child()}")
+
+
+## check
+
+node0 = Node("apple")
+node1 = Node("banana")
+node2 = Node("orange")
+node0.set_left_child(node1)
+node0.set_right_child(node2)
+
+print(f"""
+node 0: {node0.value}
+node 0 left child: {node0.left.value}
+node 0 right child: {node0.right.value}
+""")
