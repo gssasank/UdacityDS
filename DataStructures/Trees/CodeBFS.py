7f16012e6a72fb733e417ca7981fd473d7646b30
agustuz <gssasank@gmail.com>
2020-07-07 17:03:39 +0530

    Enter

--- DataStructures/Trees/CodeBFS.py
+++ DataStructures/Trees/CodeBFS.py
@@ -50,3 +50,56 @@ tree = Tree("apple")
 tree.get_root().set_left_child(Node("banana"))
 tree.get_root().set_right_child(Node("cherry"))
 tree.get_root().get_left_child().set_left_child(Node("dates"))
+
+
+from collections import deque
+class Queue():
+    def __init__(self):
+        self.q = deque()
+        
+    def enq(self,value):
+        self.q.appendleft(value)
+        
+    def deq(self):
+        if len(self.q) > 0:
+            return self.q.pop()
+        else:
+            return None
+    
+    def __len__(self):
+        return len(self.q)
+    
+    def __repr__(self):
+        if len(self.q) > 0:
+            s = "<enqueue here>\n_________________\n" 
+            s += "\n_________________\n".join([str(item) for item in self.q])
+            s += "\n_________________\n<dequeue here>"
+            return s
+        else:
+            return "<queue is empty>"
+        
+
+# BFS algorithm
+def bfs(tree):
+    visit_order = list()
+    q = Queue()
+    node = tree.get_root()
+    q.enq(node)
+    
+    while len(q)>0:
+        node = q.deq()
+        
+        visit_order.append(node)
+        
+        if node.has_left_child():
+            q.enq(node.get_left_child())
+            
+        if node.has_right_child():
+            q.enq(node.get_right_child())
+            
+    
+    print(f"visit order: {visit_order}")
+    print(q)
+    return visit_order
+
+bfs(tree)
