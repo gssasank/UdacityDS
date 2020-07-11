8d2216fbdef4e03c4388c1eb51384535a16da1cc
agustuz <gssasank@gmail.com>
2020-07-11 15:24:01 +0530

    V6.0

---
+++ Basic Algorithms/BasicAlgorithms/Tries.py
@@ -0,0 +1,67 @@
+basic_trie = {
+    # a and add word
+    'a': {'d': {'d': {'word_end': True}, 'word_end': False}, 'word_end': True},
+    # hi word
+    'h': {
+        'i': {'word_end': True},
+        'word_end': False}}
+
+
+print('Is "a"   a word: {}'.format(basic_trie['a']['word_end']))
+print('Is "ad"  a word: {}'.format(basic_trie['a']['d']['word_end']))
+print('Is "add" a word: {}'.format(basic_trie['a']['d']['d']['word_end']))
+
+
+class TrieNode(object):
+    def __init__(self):
+        self.is_word = False
+        self.children = {}
+
+
+class Trie(object):
+    def __init__(self):
+        self.root = TrieNode()
+
+    def add(self, word):
+        """
+        Add `word` to trie
+        """
+        current_node = self.root
+
+        for char in word:
+            if char not in current_node.children:
+                current_node.children[char] = TrieNode()
+
+            current_node = current_node.children[char]
+
+        current_node.is_word = True
+
+    def exists(self, word):
+        """
+        Check if word exists in trie
+        """
+        current_node = self.root
+
+        for char in word:
+            if char not in current_node.children:
+                return False
+
+            current_node = current_node.children[char]
+
+        return current_node.is_word
+
+
+word_list = ['apple', 'bear', 'goo', 'good', 'goodbye', 'goods', 'goodwill', 'gooses'  ,'zebra']
+word_trie = Trie()
+
+# Add words
+for word in word_list:
+    word_trie.add(word)
+
+# Test words
+test_words = ['bear', 'goo', 'good', 'goos']
+for word in test_words:
+    if word_trie.exists(word):
+        print('"{}" is a word.'.format(word))
+    else:
+        print('"{}" is not a word.'.format(word))
