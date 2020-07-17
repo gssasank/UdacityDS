b5d98f024950e00a4f166352b83a5fea232d5de5
augu5tus <gssasank@gmail.com>
2020-07-17 14:01:13 +0530

    V13.0

--- Basic Algorithms/SortingAlgorithms/MergeSort.py
+++ Basic Algorithms/SortingAlgorithms/MergeSort.py
@@ -1,21 +1,22 @@
 def mergesort(items):
     if len(items) == 1 or len(items) == 0:
         return items
-        
+
     mid = len(items)//2
+    
     left = items[:mid]
     right = items[mid:]
-    
+
     left = mergesort(left)
     right = mergesort(right)
-    
+
     return merge(left, right)
 
 def merge(left, right):
     merged = []
     left_index = 0
     right_index = 0
-    
+
     while left_index < len(left) and right_index < len(right):
         if left[left_index] < right[right_index]:
             merged.append(left[left_index])
@@ -35,5 +36,3 @@ test_list_3 = [97, 98, 99]
 print('{} to {}'.format(test_list_1, mergesort(test_list_1)))
 print('{} to {}'.format(test_list_2, mergesort(test_list_2)))
 print('{} to {}'.format(test_list_3, mergesort(test_list_3)))
-
-
